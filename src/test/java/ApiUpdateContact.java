3d1c6650d25fdf0bb36c8f4a68214215992e4f5c
Tuicsi <tw1xynotfound@gmail.com>
2025-03-03 19:42:42 +0200

    1.0.6

--- src/test/java/ApiUpdateContact.java
+++ src/test/java/ApiUpdateContact.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class ApiUpdateContact extends ApiCreateContact {
 
 
-    @BeforeEach
+@BeforeEach
     public void setupBeforeUpdate() {
         
         if (createdContactId == null) {
